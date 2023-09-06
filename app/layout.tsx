ec1f7bd08618bc669ae17191871795462431e4ab
lukasjoho <social@lukashoppe.com>
2023-09-06 14:39:04 +0200

    build: add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Toaster } from 'react-hot-toast';
@@ -49,6 +50,7 @@ export default function RootLayout({
             }}
           />
         </main>
+        <Analytics />
       </body>
     </html>
   );

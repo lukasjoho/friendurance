a680d513b014c887690b0794251fbbfb6640350d
lukasjoho <social@lukashoppe.com>
2023-09-06 15:38:24 +0200

    fix: remove userstats

--- app/api/prisma/route.tsx
+++
@@ -1,7 +0,0 @@
-import { prisma } from '@/lib/prisma';
-import { NextRequest, NextResponse } from 'next/server';
-
-export async function GET(req: NextRequest) {
-  await prisma.userStats.deleteMany();
-  return NextResponse.json('Deleted');
-}

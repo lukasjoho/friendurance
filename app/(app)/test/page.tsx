354b0133cbe10ed0b98468573acb2d05f077bf7f
lukasjoho <social@lukashoppe.com>
2023-09-06 11:05:03 +0200

    fix: delete test page

--- app/(app)/test/page.tsx
+++
@@ -1,9 +0,0 @@
-import { getServerUser } from '@/lib/fetcher';
-import React from 'react';
-
-const page = async () => {
-  const user = await getServerUser();
-  return <div>{JSON.stringify(user, null, 2)}</div>;
-};
-
-export default page;

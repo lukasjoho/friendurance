34e43172673459a201740db6563ad9ca10282d7f
lukasjoho <social@lukashoppe.com>
2023-09-15 17:12:29 +0200

    build: add metadata generator

--- app/(site)/feedback/page.tsx
+++ app/(site)/feedback/page.tsx
@@ -1,7 +1,17 @@
 import RegularPageLayout from '@/components/layout/RegularPageLayout';
+import { createMetaDataObject } from '@/lib/helpers/createMetaDataObject';
+import { Metadata } from 'next';
 import { Suspense } from 'react';
 import FeedbackGrid from './FeedbackGrid';
 import FeedbackGridFallback from './FeedbackGridFallback';
+
+export const metaData: Metadata = createMetaDataObject({
+  title: 'Feedback',
+  description: 'View, vote and leave feedback about the Friendurance app.',
+  slug: 'feedback',
+  imageUrl:
+    'https://res.cloudinary.com/dum2lqmke/image/upload/v1694790329/feedback_sluite.jpg',
+});
 
 const FeedbackPage = () => {
   return (

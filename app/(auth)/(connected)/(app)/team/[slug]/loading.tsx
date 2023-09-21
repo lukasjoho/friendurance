78d6bbc685521d6e90759d54818386ebe26ff2e6
lukasjoho <social@lukashoppe.com>
2023-09-21 14:33:32 +0200

    build: add loading ui for team slug

---
+++ app/(auth)/(connected)/(app)/team/[slug]/loading.tsx
@@ -0,0 +1,15 @@
+import Container from '@/components/Container';
+import { Skeleton } from '@/components/ui/skeleton';
+
+const LoadingPage = () => {
+  return (
+    <Container>
+      <div className="space-y-3 pt-3 md:space-y-8 md:pt-8">
+        <Skeleton className="h-10 w-full" />
+        <Skeleton className="aspect-video w-full" />
+      </div>
+    </Container>
+  );
+};
+
+export default LoadingPage;
